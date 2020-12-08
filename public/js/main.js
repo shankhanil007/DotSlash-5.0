3d76ff96b98bd12ec36535deaec62f3990bca080
rohitbakoliya <bakoliyarohit00@gmail.com>
2020-12-08 18:25:36 +0530

    updates -minify images -minor css changes -mata data added

--- public/js/main.js
+++ public/js/main.js
@@ -25,7 +25,7 @@
 		e.preventDefault();
 		var hash = this.hash;
 		$('html, body').animate({
-			scrollTop: $(this.hash).offset().top
+			scrollTop: $(this.hash).offset().top - 72
 		}, 800);
 	});
 
