50332b0b3fd92defc42f227956fcb09fd9f10b4b
Simply-divine <hardikvu0204@gmail.com>
2020-12-30 18:38:08 +0530

    solve registration form bug

--- index.js
+++ index.js
@@ -39,13 +39,14 @@ app.get('/coc', (req, res) => {
 });
 
 // removes none entries from array
-let sanitize = (strArr) => {
-  return strArr.filter((str) => str !== 'none');
-};
-
-// checks for duplicate strings in array
-let containsDuplicateString = (strArr) => {
-  let strSet = new Set(strArr);
+let matchString = (s1, s2, s3, s4, s5, s6) => {
+  let strSet = new Set();
+  let strArr = [s1, s2, s3, s4, s5, s6].filter((str) => {
+    return str !== 'none';
+  });
+  strArr.forEach((str) => {
+    strSet.add(str);
+  });
   if (strSet.size === strArr.length) return false;
   else return true;
 };
@@ -105,9 +106,7 @@ app.post('/api/users', async (req, res) => {
       let docEmail2 = doc.data().members[1].email2;
       let docEmail3 = doc.data().members[2].email3;
       if (
-        containsDuplicateString(
-          sanitize({ docEmail1, docEmail2, docEmail3, email1, email2, email3 })
-        )
+        matchString(docEmail1, docEmail2, docEmail3, email1, email2, email3)
       ) {
         found = true;
       }
