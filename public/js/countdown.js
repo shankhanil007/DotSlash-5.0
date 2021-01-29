63a405576706382085485b65854200faf88426a1
rohitbakoliya <bakoliyarohit00@gmail.com>
2021-01-30 00:17:55 +0530

    fixed: timer date

--- public/js/countdown.js
+++ public/js/countdown.js
@@ -1,4 +1,4 @@
-$('#timer').countdown('2021/01/31').on('update.countdown', function(event) {
+$('#timer').countdown('2021/01/30').on('update.countdown', function(event) {
     var $this = $(this).html(event.strftime(''
         + '<span>%D</span> Day%!d'
         + '<span>%H</span> Hr'
