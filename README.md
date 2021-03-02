f18c0f1f7c82b289f7488ac8dab49ef252f65f70
rohitbakoliya <bakoliyarohit00@gmail.com>
2021-03-02 17:06:27 +0530

    chore: post-hack updates
    - firestore db access removed
    - readme fixes
    - folder structure imporved

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
-# DotslashApp
+# Dotslash 4.0
+
 The official repository for the website of Dotslash Hackathon conducted by NIT Surat
 
-# Technologies Used:-
+## Technologies Used
 
-1. NodeJs
-2. Handlebars
-3. HTML5/CSS3
+- NodeJs
+- Handlebars
+- SCSS
+
+## Authors
+
+- Rohit Bakoliya - [@rohitbakoliya](https://github.com/rohitbakoliya)
+- Hardik Upadhyay - [@Simply-divine](https://github.com/Simply-divine)
--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 const express = require('express');
 const hbs = require('hbs');
-const admin = require('firebase-admin');
 const compression = require('compression');
-const serviceAccount = require('./serviceAccountKey.json');
 
 const app = express();
 const port = process.env.PORT || 5000;
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://dotslash4-8b10e.firebaseio.com',
-});
-let db = admin.firestore();
 
 // compress all responses
 app.use(compression());
@@ -20,7 +12,6 @@ app.use(compression());
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 app.use(express.json());
-app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
 // partial setup
 hbs.registerPartials(__dirname + '/views/partials');
@@ -42,144 +33,10 @@ app.get('/coc', (req, res) => {
   res.render('coc');
 });
 
-// removes none entries from array
-let matchString = (s1, s2, s3, s4, s5, s6) => {
-  let strSet = new Set();
-  let strArr = [s1, s2, s3, s4, s5, s6].filter((str) => {
-    return str !== 'none';
-  });
-  strArr.forEach((str) => {
-    strSet.add(str);
-  });
-  if (strSet.size === strArr.length) return false;
-  else return true;
-};
-
-app.post('/api/users', async (req, res) => {
-  let {
-    teamName,
-    collegeName,
-    track,
-
-    fname1,
-    dob1,
-    selGen1,
-    email1,
-    mob1,
-    git1,
-    link1,
-    twit1,
-    face1,
-    resume1,
-    tShirt1,
-
-    fname2,
-    dob2,
-    selGen2,
-    email2,
-    mob2,
-    git2,
-    link2,
-    twit2,
-    face2,
-    resume2,
-    tShirt2,
-
-    fname3,
-    dob3,
-    selGen3,
-    email3,
-    mob3,
-    git3,
-    link3,
-    twit3,
-    face3,
-    resume3,
-    tShirt3,
-    needs,
-    heardFrom,
-    firstTime,
-  } = req.body;
-  let emailSent = false;
-  let found = false;
-
-  try {
-    const docs = await db.collection('users').get();
-    docs.forEach((doc) => {
-      let docEmail1 = doc.data().members[0].email1;
-      let docEmail2 = doc.data().members[1].email2;
-      let docEmail3 = doc.data().members[2].email3;
-      if (
-        matchString(docEmail1, docEmail2, docEmail3, email1, email2, email3)
-      ) {
-        found = true;
-      }
-    });
-    if (found) {
-      res.status(503);
-      throw new Error('Duplicate Email entry found');
-    }
-    const user = await db.collection('users').add({
-      teamName,
-      collegeName,
-      track,
-      members: [
-        {
-          fname1,
-          dob1,
-          selGen1,
-          email1,
-          mob1,
-          git1,
-          link1,
-          twit1,
-          face1,
-          resume1,
-          tShirt1,
-        },
-        {
-          fname2,
-          dob2,
-          selGen2,
-          email2,
-          mob2,
-          git2,
-          link2,
-          twit2,
-          face2,
-          resume2,
-          tShirt2,
-        },
-        {
-          fname3,
-          dob3,
-          selGen3,
-          email3,
-          mob3,
-          git3,
-          link3,
-          twit3,
-          face3,
-          resume3,
-          tShirt3,
-        },
-      ],
-      needs,
-      heardFrom,
-      firstTime,
-      emailSent,
-    });
-    res.set('Sec-Fetch-Site', 'same-origin');
-    res.status(200).json({ success: true });
-  } catch (err) {
-    let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-    // console.log(statusCode);
-    res.status(statusCode).json({ success: false, error: err });
-  }
+app.get('/registration', (req, res) => {
+  res.render('form');
 });
 
-// app.get('/registration', (req, res) => {
-//   res.render('form');
-// });
+app.use('/api', require('./routes/index.routes'));
 
 app.listen(port);
