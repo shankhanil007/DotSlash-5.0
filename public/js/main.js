da1888946c54d08db8cb4fa511438bbd8b0b57c3
shankhanil007 <shankhanilborthakur@gmail.com>
2021-09-13 18:25:03 +0530

    fixes: scroll-to-top

--- public/js/main.js
+++ public/js/main.js
@@ -39,6 +39,16 @@
       800
     );
   });
+  $("a[href^='#']").on("click", function (e) {
+    e.preventDefault();
+    var hash = this.hash;
+    $("html, body").animate(
+      {
+        scrollTop: $(this.hash).offset().top - 40,
+      },
+      800
+    );
+  });
 
   // Section title animation
   $(".section-title").each(function () {
